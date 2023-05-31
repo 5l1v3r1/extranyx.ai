65c022bed815bd48522881d46dea3d4e074317b1
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-05-31 14:11:29 -0700

    Implements CrossAppDbInterface and DbDirectAccessPoint

--- ech0_mind/cli/commands/db.py
+++ ech0_mind/cli/commands/db.py
@@ -1,11 +1,11 @@
 # ech0_mind/cli/commands/db.py
 from typer import Typer, Option
-from nexus_db.core.api.nexus_db_interface import NexusDbInterface
+from nexus_db.core.api.db_direct_access_point import DbDirectAccessPoint
 app = Typer()
 
 @app.command()
 def create_db():
-    db_interface = NexusDbInterface()
+    db_interface = DbDirectAccessPoint()
     db_interface.create_db("ech0_mind")
 
 
